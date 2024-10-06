7534d9210b66873bf6d243b7c525f522dc415634
LareinaWeii <157812759+LareinaWeii@users.noreply.github.com>
2024-10-06 15:14:28 +0800

    Air Wall

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -5,26 +5,72 @@ using UnityEngine;
 public class Player1 : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Vector3 startPosition;
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");  
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        Debug.Log(isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        { 
+            Jump();
+        }
     }
 
     // FixedUpdate is called at a fixed interval and is independent of frame rate
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
     }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce * 5);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeathWall"))
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     movement.x = Input.GetAxisRaw("Horizontal");  
+    //     movement.y = Input.GetAxisRaw("Vertical");
+    // }
+
+    // // FixedUpdate is called at a fixed interval and is independent of frame rate
+    // void FixedUpdate()
+    // {
+    //     rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    // }
 }
